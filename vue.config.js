ad4fdaaad4f654172ed7c9880af0f550a9fd6b40
adrianmonteil1983 <monteiladrian@gmail.com>
2020-06-11 13:53:02 -0400

    modif config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue_resume/'
-    : '/'
+  publicPath: 'vue_resume'
   ,
   transpileDependencies: ["vuetify"],
   chainWebpack: config => {
